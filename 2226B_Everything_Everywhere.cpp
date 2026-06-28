273304cf59bc9f5e2114fe27f0cacef703f7e634
navadeep-mrn <navadeepmrn@gmail.com>
2026-06-28 19:21:58 +0530

    Sync Codeforces: 2226B - Everything Everywhere [C++23 (GCC 14-64, msys2)]

---
+++ 2226B_Everything_Everywhere.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    int rr=0;
+    for(int i=0;i<n-1;i++) if(a[i]%(a[i]-a[i+1])==0) rr++;
+    cout<<rr<<'\n';
+}
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
