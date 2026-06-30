f0c1ee34cced33e2cb663d265fe503cb41a46eec
navadeep-mrn <navadeepmrn@gmail.com>
2026-06-30 22:31:01 +0530

    Sync Codeforces: 2241A - Divide and Conquer [C++23 (GCC 14-64, msys2)]

---
+++ 2241A_Divide_and_Conquer.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+void yes(){ cout<<"YES"<<endl; }
+void no(){ cout<<"NO"<<endl; }
+ll mod=998244353;
+ 
+void solve(){
+    int x,y;cin>>x>>y;
+    if(x%y==0)  yes();
+    else no();
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+    while(t--)solve();
+}
