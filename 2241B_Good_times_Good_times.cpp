90dc8e3db982bf93b4cc571dd64cc4c67b5e5832
navadeep-mrn <navadeepmrn@gmail.com>
2026-07-01 14:45:01 +0530

    Sync Codeforces: 2241B - Good times Good times [C++23 (GCC 14-64, msys2)]

---
+++ 2241B_Good_times_Good_times.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+void yes(){ cout<<"YES"<<endl; }
+void no(){ cout<<"NO"<<endl; }
+ll mod=998244353;
+ 
+void solve(){
+    int x;
+    cin>>x;
+    int n=to_string(x).size();
+    int y=1;
+    while(n--)  y*=10;
+    cout<<y+1<<"\n";
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+    while(t--)solve();
+}
