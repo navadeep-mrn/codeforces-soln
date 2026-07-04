50d6fdd514540e36bf871e9068ed895f4616d759
navadeep-mrn <navadeepmrn@gmail.com>
2026-07-04 08:21:21 +0530

    Sync Codeforces: 1679B - Stone Age Problem [C++23 (GCC 14-64, msys2)]

---
+++ 1679B_Stone_Age_Problem.cpp
@@ -0,0 +1,6 @@
+harder problem should generally not get higher solve probability
+same-rating problem should not get unrealistic 90%+ probability
+strong tag depth should improve estimate
+cold tag should reduce confidence
+solved problems should not be recommended again
+calibration should affect cross-platform estimates
