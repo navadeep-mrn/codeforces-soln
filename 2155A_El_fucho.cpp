07728e38ba71624c06a877eb6f854677f59392ba
navadeep-mrn <navadeepmrn@gmail.com>
2026-08-16 12:03:03 +0530

    Sync Codeforces: 2155A - El fucho [C++23 (GCC 14-64, msys2)]

---
+++ 2155A_El_fucho.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+void yes(){ cout<<"YES"<<endl; }
+void no(){ cout<<"NO"<<endl; }
+#define int long long
+ 
+ 
+ 
+void solve(){
+    int n;cin>>n;
+    int x=0;
+    if(n==1) cout<<1<<endl;
+    else cout<<2*(n-1)<<endl;
+}
+ 
+    
+    
+    
+ 
+ 
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+ 
+    while(t--)solve();
+}
