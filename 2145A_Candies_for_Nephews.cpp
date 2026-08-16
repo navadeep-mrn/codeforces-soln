e61ef453295fd2ebbea817221f76935a23d0573b
navadeep-mrn <navadeepmrn@gmail.com>
2026-08-16 14:19:03 +0530

    Sync Codeforces: 2145A - Candies for Nephews [C++23 (GCC 14-64, msys2)]

---
+++ 2145A_Candies_for_Nephews.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+void yes(){ cout<<"YES"<<endl; }
+void no(){ cout<<"NO"<<endl; }
+#define int long long
+ 
+void solve(){
+    int n;cin>>n;
+    cout<<(3-n%3)%3<<endl;
+}
+ 
+ 
+ 
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+ 
+    while(t--)solve();
+}
