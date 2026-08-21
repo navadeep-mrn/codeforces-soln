d9262e45ae1f92305eaaeababdce10b68fd75509
navadeep-mrn <navadeepmrn@gmail.com>
2026-08-21 22:22:04 +0530

    Sync Codeforces: 2225D - Exceptional Segments [C++23 (GCC 14-64, msys2)]

---
+++ 2225D_Exceptional_Segments.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+void yes(){ cout<<"YES"<<endl; }
+void no(){ cout<<"NO"<<endl; }
+#define int long long
+const int MOD = 998244353;
+ 
+// Prefix XOR pattern:
+// P(i) = 1 for i = 1, 5, 9, 13, ...
+// P(i) = 0 for i = 0, 3, 7, 11, ...
+// For a segment [l, r] containing k:
+// l <= k <= r
+// Let P(i) = 1 ^ 2 ^ ... ^ i.
+// XOR(l...r) = 0 when P(l-1) = P(r).
+// Therefore, count equal prefix XOR values between:
+// left  indices [0, k-1]
+// right indices [k, n].
+vector<int> fact;
+void solve(){
+    int n,k;cin>>n>>k;
+    int ca=0,cb=0,da=0,db=0;
+    ca=(k-1>=1 ? (k-2)/4+1 : 0);
+    cb=(n>=1 ? (n-1)/4+1 : 0)-ca;
+    da=1+(k-1>=3 ? (k-4)/4+1 : 0);
+    db=(1+(n>=3 ? (n-3)/4+1 : 0))-da;
+    int f=(ca%MOD)*(cb%MOD)%MOD;
+    f+=(da%MOD)*(db%MOD)%MOD;
+    f=f%MOD;
+    cout<<f<<endl;
+}
+ 
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    
+}
