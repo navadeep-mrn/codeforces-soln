ece2d0afdf947b4600ca15b8d4db0b53cfef8b8b
navadeep-mrn <navadeepmrn@gmail.com>
2026-08-22 15:48:02 +0530

    Sync Codeforces: 1916B - Two Divisors [C++23 (GCC 14-64, msys2)]

---
+++ 1916B_Two_Divisors.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+void yes(){ cout<<"YES"<<endl; }
+void no(){ cout<<"NO"<<endl; }
+#define int long long
+const ll MOD = 1e9 + 7;
+ 
+int lcm(int a, int b) {
+    return (a / __gcd(a, b)) * b;
+}
+void solve(){
+    int x,y;cin>>x>>y;
+    int z;
+    if(y%x==0){
+        z=y*y/x;
+ 
+    }
+    else{
+        z=(x*y)/__gcd(x,y);
+    }
+    cout<<z<<endl;
+}
+ 
+ 
+ 
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+ 
+    while(t--)solve();
+}
