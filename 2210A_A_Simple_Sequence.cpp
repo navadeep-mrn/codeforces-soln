fd26924aa9d3fd846fec1aca620256d08a1ccbe8
navadeep-mrn <navadeepmrn@gmail.com>
2026-09-21 14:35:05 +0530

    Sync Codeforces: 2210A - A Simple Sequence [C++23 (GCC 14-64, msys2)]

---
+++ 2210A_A_Simple_Sequence.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+ 
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        for(int i=n;i>=1;i--)
+            cout<<i<<" ";
+        cout<<"\n";
+    }
+ 
+    return 0;
+}
