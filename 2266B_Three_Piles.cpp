dbe5c19070674499b31304ecb9b7376e50a1e203
navadeep-mrn <navadeepmrn@gmail.com>
2026-09-21 23:07:07 +0530

    Sync Codeforces: 2266B - Three Piles [C++23 (GCC 14-64, msys2)]

---
+++ 2266B_Three_Piles.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+void yes(){ cout<<"YES"<<endl; }
+void no(){ cout<<"NO"<<endl; }
+#define int long long
+const ll MOD = 1e9 + 7;
+ 
+int lcm(int a, int b) {
+    return (a / __gcd(a, b)) * b;
+}
+void solve(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    int d=abs(a-b);
+    if(a>=b){
+        d+=c;
+        cout<<d<<endl;
+    }
+    else{
+        d=max(d,c-d);
+        cout<<d<<endl;
+    }
+    
+}
+ 
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin>>t;
+ 
+    while(t--)solve();
+}
